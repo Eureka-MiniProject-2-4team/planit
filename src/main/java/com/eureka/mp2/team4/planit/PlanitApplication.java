d83bb713fe93ab88c593aba5eacac6b52ce635d7
suhyun <suhyun9764@naver.com>
2025-05-09 01:28:00 +0900

    build: mapper 관련 설정

--- src/main/java/com/eureka/mp2/team4/planit/PlanitApplication.java
+++ src/main/java/com/eureka/mp2/team4/planit/PlanitApplication.java
@@ -1,9 +1,11 @@
 package com.eureka.mp2.team4.planit;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.eureka.mp2.team4.planit")
 public class PlanitApplication {
 
     public static void main(String[] args) {
