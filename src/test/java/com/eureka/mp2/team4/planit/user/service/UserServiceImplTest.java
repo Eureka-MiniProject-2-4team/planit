45d92d0ec81f722e6fd97dfcd1e5fbf68f008385
suhyun <suhyun9764@naver.com>
2025-05-13 11:38:43 +0900

    feat : 닉네임 변경 기능 구현 (#24)

--- src/test/java/com/eureka/mp2/team4/planit/user/service/UserServiceImplTest.java
+++ src/test/java/com/eureka/mp2/team4/planit/user/service/UserServiceImplTest.java
@@ -1,7 +1,11 @@
 package com.eureka.mp2.team4.planit.user.service;
 
+import com.eureka.mp2.team4.planit.common.ApiResponse;
+import com.eureka.mp2.team4.planit.common.Result;
+import com.eureka.mp2.team4.planit.common.exception.DatabaseException;
 import com.eureka.mp2.team4.planit.common.exception.InternalServerErrorException;
 import com.eureka.mp2.team4.planit.common.exception.NotFoundException;
+import com.eureka.mp2.team4.planit.user.dto.UpdateNickNameRequestDto;
 import com.eureka.mp2.team4.planit.user.dto.UserDto;
 import com.eureka.mp2.team4.planit.user.dto.response.UserResponseDto;
 import com.eureka.mp2.team4.planit.user.enums.UserRole;
@@ -12,9 +16,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataAccessException;
 
-import static com.eureka.mp2.team4.planit.user.constants.Messages.NOT_FOUND_USER;
+import static com.eureka.mp2.team4.planit.user.constants.Messages.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -75,5 +81,35 @@ public class UserServiceImplTest {
 
         assertThrows(InternalServerErrorException.class,
                 () -> userService.getMyPageData(userId));
+    }
+
+    @Test
+    @DisplayName("닉네임 수정 성공 테스트")
+    void updateNickName_success() {
+        // given
+        String userId = "user-123";
+        UpdateNickNameRequestDto dto = new UpdateNickNameRequestDto("newNickname");
+
+        // when
+        ApiResponse response = userService.updateNickName(userId, dto);
+
+        // then
+        assertEquals(Result.SUCCESS, response.getResult());
+        assertEquals(UPDATE_NICKNAME_SUCCESS, response.getMessage());
+        verify(userMapper, times(1)).updateNickName(userId, "newNickname");
+    }
+
+    @Test
+    @DisplayName("닉네임 수정 실패 테스트 - DataAccessException 발생")
+    void updateNickName_dataAccessException() {
+        // given
+        String userId = "user-123";
+        UpdateNickNameRequestDto dto = new UpdateNickNameRequestDto("newNickname");
+
+        doThrow(new DataAccessException(UPDATE_NICKNAME_FAIL) {}).when(userMapper).updateNickName(userId, "newNickname");
+
+        // when & then
+        assertThrows(DatabaseException.class, () -> userService.updateNickName(userId, dto));
+        verify(userMapper, times(1)).updateNickName(userId, "newNickname");
     }
 }
