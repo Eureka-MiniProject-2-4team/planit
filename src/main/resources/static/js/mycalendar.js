ca0aa4ea4f95d114f496a9b63ab24853fad4f5a8
mike7643 <developerJYumin@gmail.com>
2025-05-16 01:59:23 +0900

    refator : todo 리스트 html,css,js

--- src/main/resources/static/js/mycalendar.js
+++ src/main/resources/static/js/mycalendar.js
@@ -666,7 +666,7 @@ function initializeDatePicker() {
 
     // Date picker for detail form
     $("#detail-date-display").datepicker({
-        dateFormat: 'yy년 mm월 dd일 (D)',
+        dateFormat: 'mm / dd (D)',
         dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
         dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
         monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
